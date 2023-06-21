8946d211e16486fe66323f139ef2854f92719d3c
Tyrus Skipper <tskip06@gmail.com>
2023-06-21 12:43:23 -0400

    test commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "paste_your_api_url_here"
+    API_URL: "https://c4a12599-4aaf-422b-9a71-1b5fa388f296.serverhub.practicum-services.com/"
 }
