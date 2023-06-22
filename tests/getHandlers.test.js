8fc62d8eaafac6d3f11caeb7db5b3921ed04c6f2
Tyrus Skipper <tskip06@gmail.com>
2023-06-22 10:37:04 -0400

    initial update to get and post test files

--- tests/getHandlers.test.js
+++ tests/getHandlers.test.js
@@ -1,10 +1,34 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
+let actualStatus;
 
-test('', async () => {
+test('status should be 200', async () => {
 	try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`);
+		//make request
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses`);
+		//extract response code
+		actualStatus = response.status;
 	} catch (error) {
 		console.error(error);
 	}
+
+	//check status code
+	expect(actualStatus).toBe(200);
+});
+
+test('status should be 404 when using an invalid API URL', async () => {
+	try {
+		//make request
+		const response = await fetch(`${config.INVALID_API_URL}/api/v1/warehouses`);
+		//extract response code
+		actualStatus = response.status;
+	} catch (error) {
+		console.error(error);
+	}
+
+	//print status code
+	//console.log(actualStatus);
+
+	//check status code
+	expect(actualStatus).toBe(404);
 });
--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -1,20 +1,135 @@
 // eslint-disable-next-line no-undef
 const config = require('../config');
+let actualStatus;
 
-const requestBody = {
-    // put your body here
+const requestBody = {"products":[
+    {
+		"id": 5,
+		"quantity": 1
+	},
+	{
+		"id": 4,
+		"quantity": 5
+	}
+]
 }
 
-test('', async () => {
+test('status should be 200', async () => {
     try {
-		const response = await fetch(`${config.API_URL}/your/endpoint`, {
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses/check`, {
 			method: 'POST',
 			headers: {
 			'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(requestBody)
 		});
+
+		//get status code
+		actualStatus=response.status;
+
 	} catch (error) {
 		console.error(error);
 	}
+
+	//check status code
+	expect(actualStatus).toBe(200);
+
 });
+
+test('status should be 404', async () => {
+    try {
+		const response = await fetch(`${config.INVALID_API_URL}/api/v1/warehouses/check`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(requestBody)
+		});
+
+		//get status code
+		actualStatus=response.status;
+
+	} catch (error) {
+		console.error(error);
+	}
+
+	//check status code
+	expect(actualStatus).toBe(404);
+
+});
+
+const invalidRequestBody = {"products":[
+    {
+		"id": "zero",
+		"quantity": 1
+	},
+	{
+		"id": 4,
+		"quantity": 5
+	}
+]}
+
+test('With a string in the id field the status should be 504', async () => {
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses/check`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(invalidRequestBody)
+		});
+
+		//get status code
+		actualStatus=response.status;
+
+	} catch (error) {
+		console.error(error);
+	}
+
+	//test print the status code
+	//console.log(actualStatus);
+
+	//check status code
+	expect(actualStatus).toBe(504);
+	
+
+});
+
+const invalidRequestBody2 = {"products":[
+    {
+		"id": 0,
+		"quantity": "one"
+	},
+	{
+		"id": 4,
+		"quantity": 5
+	}
+]}
+
+test('With a string in the quantity field the status should be 504', async () => {
+    try {
+		const response = await fetch(`${config.API_URL}/api/v1/warehouses/check`, {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(invalidRequestBody)
+		});
+
+		//get status code
+		actualStatus=response.status;
+
+	} catch (error) {
+		console.error(error);
+	}
+
+	//test print the status code
+	//console.log(actualStatus);
+
+	//check status code
+	expect(actualStatus).toBe(504);
+	
+
+});
+
+
