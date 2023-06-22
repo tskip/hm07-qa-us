01c993cb0dd249571d19f0c47864cf3ded3b6062
Tyrus Skipper <tskip06@gmail.com>
2023-06-22 10:38:07 -0400

    update to config.js file

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://c4a12599-4aaf-422b-9a71-1b5fa388f296.serverhub.practicum-services.com/"
+    API_URL: "https://9a81206f-7cf3-48a0-881f-306d1323fdd4.serverhub.practicum-services.com",
+    INVALID_API_URL: "https://52b925a1-f5b8-4250-93c2-42fc0a5d9064.serverhub.practicum-services.com"
 }
