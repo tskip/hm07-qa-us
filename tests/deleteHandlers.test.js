ec3fbb1f2bfcd14d013ed8f217e79f64f705b3ae
Tyrus Skipper <tskip06@gmail.com>
2023-06-24 18:37:59 -0400

    added parse test for DELETE

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-undef
 const { response } = require('carthage/core/required/api');
 const config = require('../config');
-let actualStatus;
+
 
 test('With a valid URL status should be 200', async () => {
-    try {
+    let actualStatus;
+	try {
 		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
 			method: 'DELETE',
 		});
@@ -16,15 +17,13 @@ test('With a valid URL status should be 200', async () => {
 		console.error(error);
 	}
 
-	//print status code
-	//console.log(actualStatus);
-	
 	//check status code
 	expect(actualStatus).toBe(200);
 });
 
 test('With a invalid URL status should be 404', async () => {
-    try {
+    let actualStatus;
+	try {
 		const response = await fetch(`${config.INVALID_API_URL}/api/v1/kits/2`, {
 			method: 'DELETE',
 		});
@@ -36,15 +35,13 @@ test('With a invalid URL status should be 404', async () => {
 		console.error(error);
 	}
 
-	//print status code
-	//console.log(actualStatus);
-	
 	//check status code
 	expect(actualStatus).toBe(404);
 });
 
 test('Status should be 500 if kit id is a string', async () => {
-    try {
+    let actualStatus;
+	try {
 		const response = await fetch(`${config.API_URL}/api/v1/kits/one`, {
 			method: 'DELETE',
 		});
@@ -55,10 +52,62 @@ test('Status should be 500 if kit id is a string', async () => {
 	} catch (error) {
 		console.error(error);
 	}
-
-	//print status code
-	//console.log(actualStatus);
 	
 	//check status code
 	expect(actualStatus).toBe(500);
 });
+
+test('Deleting a cart that does not exist should return 404', async () => {
+    let actualStatus;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/orders/2`, {
+			method: 'DELETE',
+		});
+
+	//get status code
+	actualStatus = response.status;
+
+	} catch (error) {
+		console.error(error);
+	}
+	
+	//check status code
+	expect(actualStatus).toBe(404);
+});
+
+test('Deleting a kit that exist should return 200', async () => {
+    let actualStatus;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+			method: 'DELETE',
+		});
+
+	//get status code
+	actualStatus = response.status;
+
+	} catch (error) {
+		console.error(error);
+	}
+	
+	//check status code
+	expect(actualStatus).toBe(200);
+});
+
+test('Body should contain TRUE boolean when kit is successfully deleted', async () => {
+    let actualResponseBody;
+	try {
+		const response = await fetch(`${config.API_URL}/api/v1/kits/2`, {
+			method: 'DELETE',
+		});
+
+	//extract response body
+	actualResponseBody = await response.json();
+
+	} catch (error) {
+		console.error(error);
+	}
+
+	expect(actualResponseBody.ok).toBeTruthy();
+
+});
+
